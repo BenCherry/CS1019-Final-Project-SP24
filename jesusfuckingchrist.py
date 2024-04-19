bfeaee3df7b10ce2fbb6b1e5ace109efa1e78575
BenCherry <36824309+BenCherry@users.noreply.github.com>
2024-04-18 21:48:11 -0600

    Add files via upload

--- jesusfuckingchrist.py
+++ jesusfuckingchrist.py
@@ -5,32 +5,32 @@ Created on Thu Apr 18 18:04:57 2024
 @author: cherrbear
 """
 
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 17 16:23:59 2024
-
-@author: CherrBear
-"""
 import random
 
-my_list1 = [
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11
-    ]
+my_list1 = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+            2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+            2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+            2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11
+            ] 
 player_hand = [] #creates empty player list to store cards 
 dealer_hand = []
 
-def deal_hand(hand): # gives the first 2 cards of the hand
+
+#INPUT player_hand and dealer_hand lists as arguments
+#PROCESSING random item removed from list, assigned to player/dealer list
+#OUTPUT player or dealer list with the 2 random items
+def deal_hand(hand): # 
     if len(hand) == 0:    
-        for card in range(2):      # argument is player_hand or dealer_hand RE
+        for card in range(2): 
             card_index = random.randrange(len(my_list1))
             hand.append(my_list1.pop(card_index))
     return hand
-            
+
+#INPUT player/dealer_hand lists as arguments           
+#PROCESSING checks for items in list, random item removed, assign to list
+#OUTPUT adds random item to player/dealer, print statement if empty
 def hit(x):
-    if my_list1:
+    if my_list1: # checks for items in list, 1 or more items it executes
         card_index = random.randrange(len(my_list1))
         x.append(my_list1.pop(card_index))
     else:
@@ -38,6 +38,9 @@ def hit(x):
     return x
         
 
+#INPUT no arguments, starts game when called
+#PROCESSING calls deal_hand func, infinite loop, 
+#OUTPUT 
 def start_game():
     play = input("Pres 'Y' to play, or 'Q' to exit: ")
     if play == 'y':
@@ -61,3 +64,31 @@ def start_game():
                 
     elif play == 'q':
         print('Thanks for stopping in!')
+    
+    house_hand()
+    winning_hand()
+#INPUT
+#PROCESSING
+#OUTPUT
+def house_hand():
+    if sum(dealer_hand) < 17:
+        while sum(dealer_hand) < 17:
+            hit(dealer_hand)
+            print(f'Dealer has {sum(dealer_hand)}')
+    elif sum(dealer_hand) > 21:
+        print(f'Dealer busts with {sum(dealer_hand)}')        
+
+#INPUT
+#PROCESSING
+#OUTPUT
+def winning_hand():
+    if sum(dealer_hand) > sum(player_hand):
+        print(f'You have a {sum(player_hand)}, ' 
+              f'dealer has a {sum(dealer_hand)}, dealer wins.')
+    elif sum(dealer_hand) < sum(player_hand):
+        print(f'Dealer has a {sum(dealer_hand)}, '
+              f' you win with a {sum(player_hand)}')
+    elif sum(dealer_hand) == sum(player_hand):
+        print("A push is a win")
+
+start_game()
