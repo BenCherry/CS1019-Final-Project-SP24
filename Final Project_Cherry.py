7d3a3821c487e70faaa3751b1e80e5143d94b1c8
Ryan Cherry <36824309+BenCherry@users.noreply.github.com>
2024-07-31 17:23:50 -0600

    Update Final Project_Cherry.py
    Moved the description of the program from the top docstring(?) to the README

--- Final Project_Cherry.py
+++ Final Project_Cherry.py
@@ -6,22 +6,6 @@ Created on Sat May  4 13:32:57 2024
 
 Main function that runs the program.
 
-This is a blakjack simulator that allows the user to play single deck 
-blackjack.
-
-Cards are randomly drawn from a deck , and added to the player and dealer lists.
-
-Aces are handled by checking the sum of the player/dealer list, and if over 21,
-and an Ace (11) is present in the list, subtracting 10 and saving the value to
-a local variable as the score. This solves the issue of an Ace being counted as
-an 11 or a 1, whichever is more beneficial.
-
-Storing the score this way allows for multiple aces (a list total over 21).
-
-Scores are then compared and the result is output.
-
-Cards are then collected and moved to the discard, or shuffled as required.
-
 """
 
 
